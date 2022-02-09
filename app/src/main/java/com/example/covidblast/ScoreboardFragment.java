1c5def6758cf8a5e3549a0433b2e985337967006
Daniel Israelov <svhrkz@gmail.com>
2022-02-09 10:31:21 +0200

    created xml shape for coins textview

--- app/src/main/java/com/example/covidblast/ScoreboardFragment.java
+++ app/src/main/java/com/example/covidblast/ScoreboardFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
